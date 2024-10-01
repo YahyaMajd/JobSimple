39619b06d57889a93fab1d84ea5a34033e7ab8f4
YahyaMajd <112823817+YahyaMajd@users.noreply.github.com>
2024-09-30 18:33:07 -0700

    Initial Commit

---
+++ JobSimple/JobSimpleApp.swift
@@ -0,0 +1,17 @@
+//
+//  JobSimpleApp.swift
+//  JobSimple
+//
+//  Created by Yahya on 2024-09-30.
+//
+
+import SwiftUI
+
+@main
+struct JobSimpleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
